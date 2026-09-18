e416454a4e428c6e9a4d0103a230cb3012dc3253
SoumenC-84 <soumen.chakraborty29@gmail.com>
2026-09-18 21:08:32 +0530

    Update backend and frontenf workflow

---
+++ frontend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+const distPath = path.join(__dirname, "dist");
+
+app.use(express.static(distPath));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+});
+
+app.listen(port, "0.0.0.0", () => {
+    console.log(`React application running on port ${port}`);
+});
